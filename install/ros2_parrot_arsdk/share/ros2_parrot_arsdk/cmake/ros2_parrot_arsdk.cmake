43eafbd7ac62ddd8876944a19d86bc4ae72289ca
Max Gardenswartz <134117510+mgardenswartz@users.noreply.github.com>
2025-08-18 11:18:15 -0400

    Removed ignored files from cache

--- install/ros2_parrot_arsdk/share/ros2_parrot_arsdk/cmake/ros2_parrot_arsdk.cmake
+++
@@ -1,3 +0,0 @@
-set(ARSDK_LIB_DIR /home/max/bebop_ws/install/ros2_parrot_arsdk/lib)
-set(ARSDK_LIBS arcommands arcontroller ardatatransfer ardiscovery armavlink armedia arnetworkal arnetwork arsal arstream2 arstream arupdater arutils)
-set(ARSDK_HASH 1ff5bdc5458627c12eb22e1dd1814cff25778f31)
