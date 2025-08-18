a1109f341bf31cc278d01345d61eb9a6866caa93
Max Gardenswartz <max.gardenswartz@proton.me>
2025-08-18 15:55:22 -0400

    Does it work?

--- src/bebop_client/src/bebop_client_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "BebopClient.hpp"
-
-int main(int argc, char ** argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<BebopClient>());
-    rclcpp::shutdown();
-    
-    return 0;
-}
