015073a20cdf77b5da9db33379b21ff4bd614caf
Max Gardenswartz <max.gardenswartz@proton.me>
2025-12-15 23:37:21 -0500

    Renamed variables

--- mocap_filters/include/mocap_filters/LevantFilter.hpp
+++ mocap_filters/include/mocap_filters/LevantFilter.hpp
@@ -12,27 +12,28 @@ public:
         alpha_ = 1.1 * C;
         lambda_ = 1.5 * std::sqrt(C);
         x_hat_.setZero();
-        u1_.setZero();
-        v_out_.setZero();
+        rho_.setZero();
+        rho_dot_.setZero();
     }
 
-    void propagate(const Eigen::Vector3d& f_t) {
-        Eigen::Vector3d e = f_t - x_hat_;
+    // Hard coded the number of states to 3
+    void propagate(const Eigen::Vector3d& x_meas) {
+        Eigen::Vector3d x_tilde = x_meas - x_hat_;
         for (int i = 0; i < 3; ++i) {
-            double s = (e(i) > 0) - (e(i) < 0);
-            double abs_e = std::abs(e(i));
-            u1_(i) += alpha_ * s * dt_;
-            v_out_(i) = u1_(i) + lambda_ * std::sqrt(abs_e) * s;
-            x_hat_(i) += v_out_(i) * dt_;
+            double sgn_x_tilde = (x_tilde(i) > 0) - (x_tilde(i) < 0);
+            double abs_x_tilde = std::abs(x_tilde(i));
+            rho_(i) += abs_x_tilde * sgn_x_tilde * dt_;
+            rho_dot_(i) = rho_(i) + lambda_ * std::sqrt(abs_x_tilde) * sgn_x_tilde;
+            x_hat_(i) += rho_dot_(i) * dt_;
         }
     }
 
     Eigen::Vector3d getPosition() const { return x_hat_; }
-    Eigen::Vector3d getVelocity() const { return v_out_; }
+    Eigen::Vector3d getVelocity() const { return rho_dot_; }
 
 private:
     double dt_, alpha_, lambda_;
-    Eigen::Vector3d x_hat_, u1_, v_out_;
+    Eigen::Vector3d x_hat_, rho_, rho_dot_;
 };
 
 } // namespace mocap_filters
