93fdab5131cfef2a4889998ca0ef58d9a4a7474a
uflautonomypark <uflautonomypark@gmail.com>
2025-12-19 12:49:33 -0500

    move to own repos

--- ncr_lab_viz/src/ncr_lab_viz_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "NCRLabViz.hpp"
-
-int main(int argc, char ** argv) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<NCRLabViz>());
-    rclcpp::shutdown();
-    return 0;
-}
